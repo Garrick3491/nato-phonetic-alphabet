b819ebd61bd617f303f219a71fe657edb9a4d601
Simon Garrick <s.garrick1991@gmail.com>
2021-09-14 07:17:04 +0100

    KeyError handling

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-
-#Looping through dictionaries:
-# for (key, value) in student_dict.items():
-    #Access key and value
-    # pass
-
 import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    # pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
@@ -26,9 +6,16 @@ panda = pandas.read_csv('./nato_phonetic_alphabet.csv')
 
 phonetic_alphabet = {row.letter:row.code for (index, row) in panda.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input_name = input("Please enter your name: ").upper()
 
-phonetic_alphabet_list = [phonetic_alphabet[letter] for letter in list(user_input_name)]
+def generate_phonetic():
+    user_input_name = input("Please enter your name: ").upper()
+    try:
+        phonetic_alphabet_list = [phonetic_alphabet[letter] for letter in list(user_input_name)]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_alphabet_list)
 
-print(phonetic_alphabet_list)
+
+generate_phonetic()
